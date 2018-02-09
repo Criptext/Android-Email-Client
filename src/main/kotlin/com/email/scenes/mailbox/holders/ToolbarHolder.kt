a243060bd7853616eb0909dc2d1fa3dd28ed2510
Sebastián Cáceres <ascacere92@gmail.com>
2018-02-09 11:50:38 -0500

    Added new items, customizing UI

--- src/main/kotlin/com/email/scenes/mailbox/holders/ToolbarHolder.kt
+++ src/main/kotlin/com/email/scenes/mailbox/holders/ToolbarHolder.kt
@@ -4,6 +4,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import com.email.R
+import com.email.utils.ui.Tint
 
 /**
  * Created by sebas on 2/6/18.
@@ -13,15 +14,20 @@ class ToolbarHolder(val view: View) {
     private val title: TextView
     private val numberEmails: TextView
     private val navButton: ImageView
+    private val backButton: ImageView
     init {
         title = view.findViewById(R.id.mailbox_toolbar_title)
         numberEmails = view.findViewById(R.id.mailbox_number_emails)
         navButton = view.findViewById(R.id.mailbox_nav_button)
+        backButton = view.findViewById(R.id.mailbox_back_button)
+
+        Tint.addTintToImage(view.context, backButton)
     }
 
     fun showMultiModeBar(selectedThreadsQuantity: Int){
         navButton.visibility = View.GONE
         numberEmails.visibility = View.GONE
+        backButton.visibility = View.VISIBLE
         title.text = selectedThreadsQuantity.toString()
     }
 
@@ -29,6 +35,7 @@ class ToolbarHolder(val view: View) {
 
         navButton.visibility = View.VISIBLE
         numberEmails.visibility = View.VISIBLE
+        backButton.visibility = View.GONE
         title.text = "INBOX"
     }
 
