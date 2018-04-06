76e83ac1e6839b37f1ec76b929e83d794f499e61
Sebastián Cáceres <ascacere92@gmail.com>
2018-04-06 10:41:40 -0500

    removed Okhttp client instance from MailboxAPIClient.

--- src/main/kotlin/com/email/scenes/mailbox/data/MailboxAPIClient.kt
+++ src/main/kotlin/com/email/scenes/mailbox/data/MailboxAPIClient.kt
@@ -2,8 +2,6 @@ package com.email.scenes.mailbox.data
 
 import com.email.api.ApiCall
 import com.email.scenes.composer.data.PostEmailBody
-import okhttp3.OkHttpClient
-import java.util.concurrent.TimeUnit
 
 /**
  * Created by sebas on 3/20/18.
@@ -12,16 +10,11 @@ import java.util.concurrent.TimeUnit
 
 class MailboxAPIClient(private val token: String) {
 
-    private val client = OkHttpClient().
-            newBuilder().
-            connectTimeout(20, TimeUnit.SECONDS).
-            readTimeout(20, TimeUnit.SECONDS).
-            build()
 
 
     fun getPendingEvents(): String {
         val request = ApiCall.getPendingEvents(token = token)
-        return ApiCall.executeRequest(client, request)
+        return ApiCall.executeRequest(request)
     }
 
     fun getBodyFromEmail(uuid: String): String {
@@ -29,17 +22,17 @@ class MailboxAPIClient(private val token: String) {
                 token = token,
                 uuid= uuid
                 )
-        return ApiCall.executeRequest(client, request)
+        return ApiCall.executeRequest(request)
     }
 
     fun findKeyBundles(recipients: List<String>, knownAddresses: Map<String, List<Int>>): String {
         val request = ApiCall.findKeyBundles(token, recipients, knownAddresses)
-        return ApiCall.executeRequest(client, request)
+        return ApiCall.executeRequest(request)
     }
 
     fun postEmail(postEmailBody: PostEmailBody): String {
         val request = ApiCall.postEmail(token, postEmailBody)
-        return ApiCall.executeRequest(client, request)
+        return ApiCall.executeRequest(request)
     }
 
 }
