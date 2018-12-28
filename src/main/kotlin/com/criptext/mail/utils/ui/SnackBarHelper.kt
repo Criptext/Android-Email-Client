52498cbdf6ff0f878d4697f3deefb7e018b7f1eb
Jorge Blacio <jorgeblacio@gmail.com>
2018-12-28 18:49:44 -0500

    Release Dark Theme

--- src/main/kotlin/com/criptext/mail/utils/ui/SnackBarHelper.kt
+++ src/main/kotlin/com/criptext/mail/utils/ui/SnackBarHelper.kt
@@ -1,15 +1,22 @@
 package com.criptext.mail.utils.ui
 
 import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
 import android.view.View
 import com.criptext.mail.R
 import com.criptext.mail.utils.getColorFromAttr
+import android.widget.TextView
+
+
 
 class SnackBarHelper{
     companion object {
         fun show(view: View, message: String){
             val sb = Snackbar.make(view, message, Snackbar.LENGTH_LONG)
             sb.view.setBackgroundColor(view.context.getColorFromAttr(R.attr.criptextSnackBarBackground))
+            val snackbarTextId = android.support.design.R.id.snackbar_text
+            val textView = sb.view.findViewById(snackbarTextId) as TextView
+            textView.setTextColor(ContextCompat.getColor(view.context, R.color.white))
             sb.show()
         }
     }
